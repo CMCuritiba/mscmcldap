79bbb629122fe62712ce5d3da61bccafe992f739
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2017-06-12 18:23:21 -0300

    correcao arquivo travis

--- config/settings/travis.py
+++ config/settings/travis.py
@@ -12,6 +12,8 @@ from __future__ import absolute_import, unicode_literals
 
 import environ, os
 
+env = environ.Env(DEBUG=(bool, False),)
+
 ROOT_DIR = environ.Path(__file__) - 3  # (chamados-cmc/config/settings/base.py - 3 = chamados-cmc/)
 APPS_DIR = ROOT_DIR.path('mscmcldap')
 
