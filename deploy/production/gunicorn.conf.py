6545e8a3aa3901e61adba71df39e00bb476f0794
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2017-10-16 15:27:56 -0200

    teste

--- deploy/production/gunicorn.conf.py
+++ deploy/production/gunicorn.conf.py
@@ -1,3 +1,4 @@
-bind = 'unix:/var/run/webapps/mscmcldap/gunicorn.sock'
+#bind = 'unix:/var/run/webapps/mscmcldap/gunicorn.sock'
+bind = 'unix:///var/run/webapps/mscmcldap/gunicorn.sock'
 workers = 2  
 timeout = 30  
