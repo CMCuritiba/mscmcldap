c749070c35f55ae40b473d8189b268ec4fd9aa54
aleodoni <aleodoni@gmail.com>
2019-02-20 10:22:04 -0300

    data reuniões fixa para 25/02/2019

--- fabfile.py
+++ fabfile.py
@@ -220,8 +220,14 @@ def cria_links():
 
 @task
 def restart_nginx_supervisor():
+	sudo('supervisorctl stop celery')
+	# ainda não foi resolvido no celery processo para matar os filhos via supervisor
+	# só mata o processo 'pai', deixando os filhos zumbis
+	# o comando abaixo detona os zumbis na mão
+	sudo('pkill -f {}'.format('/usr/share/envs/chamados/bin/celery'))
 	sudo('supervisorctl reread')
-	sudo('supervisorctl restart {}'.format(ENV_NAME))
+	sudo('supervisorctl restart mscmc')
+	sudo('supervisorctl start celery')
 	sudo('service nginx restart')
 
 @task
--- mscmcldap/api/views/spl.py
+++ mscmcldap/api/views/spl.py
@@ -32,7 +32,7 @@ class CargosMesaSerializer(serializers.ModelSerializer):
 def spl_reuniao_comissao(request):
     reunioes_json = []
     #hoje = datetime.datetime.now()
-    hoje = datetime.date(2019,02,25)
+    hoje = datetime.date(2019,2,25)
     request.session.flush()
 
     reunioes = v_spl_reuniao_comissao.objects.filter(rec_data=hoje)
