fbdf70b615731dbc96f7ed71b37f8153abda4391
Alexandre Odoni <aleodoni@gmail.com>
2026-05-06 16:15:44 -0300

    bug: Atualiza settings prod

--- mscmcldap/api/views/spl.py
+++ mscmcldap/api/views/spl.py
@@ -23,20 +23,6 @@ from ..mocks.projetos_reuniao import get_projeto_mock, get_projeto_reuniao_mock,
 logger = logging.getLogger(__name__)
 
 
-class VereadorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = v_spl_vereador
-        fields = ('ver_id', 'matricula', 'ver_sexo', 'ut_id', 'ini_nome', 'ini_ativa', 'ver_nome_completo',
-                  'ini_codigo_prefeitura', 'ver_site', 'ver_biografia', 'ver_redes_sociais', 'ver_fone_principal',
-                  'ver_fones', 'ver_legislaturas', 'ver_localizacao', 'ver_partido', 'arq_id', 'arq_id_biografia')
-
-
-class CargosMesaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = v_spl_cargos_mesa
-        fields = ('matricula', 'ini_nome', 'crg_nome', 'crg_ordem')
-
-
 @api_view(['GET'])
 def spl_reuniao_comissao(request):
     reunioes_json = []
@@ -58,7 +44,6 @@ def spl_projeto_reuniao(request, pac_id, par_id):
     dados = get_projeto_reuniao(pac_id, par_id)
     return JsonResponse(dados, safe=False)
 
-
 # ---------------------------------------------------------------------------------------------------
 # api que retorna as reuniões dentro de um range de datas
 # ---------------------------------------------------------------------------------------------------
@@ -67,32 +52,6 @@ def spl_reuniao_comissao_range(request, data_inicio, data_fim):
     dados = get_reunioes_comissao_range(data_inicio, data_fim)
     return JsonResponse(dados, safe=False)
 
-
-# @api_view(['GET'])
-# def spl_vereadores(request):
-#     vereadores = v_spl_vereador.objects.all().order_by('ini_nome')
-#     serializer = VereadorSerializer(vereadores, many=True)
-#     return Response(serializer.data)
-
-
-# @api_view(['GET'])
-# def spl_vereador_matricula(request, matricula):
-#     vereador = None
-#     try:
-#         vereador = v_spl_vereador.objects.get(matricula=matricula)
-#         serializer = VereadorSerializer(vereador, many=False)
-#         return Response(serializer.data)
-#     except vereador.DoesNotExist:
-#         raise Http404
-
-
-# @api_view(['GET'])
-# def spl_cargos_mesa(request):
-#     cargos_mesa = v_spl_cargos_mesa.objects.all().order_by('crg_ordem')
-#     serializer = CargosMesaSerializer(cargos_mesa, many=True)
-#     return Response(serializer.data)
-
-
 # ---------------------------------------------------------------------------------------------------
 # api que retorna o rec_id a partir da pac_id
 # ---------------------------------------------------------------------------------------------------
@@ -100,7 +59,6 @@ def spl_reuniao_comissao_range(request, data_inicio, data_fim):
 def spl_get_rec_id(request, pac_id):
     dados = get_rec_id(pac_id)
     return JsonResponse(dados, safe=False)
-
 
 # ---------------------------------------------------------------------------------------------------
 # api que retorna os dados da reuniao atraves do rec_id
