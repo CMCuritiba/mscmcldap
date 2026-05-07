bf449a8601588e05103da0d5818919c7b69f2b72
Alexandre Odoni <aleodoni@gmail.com>
2026-05-07 09:50:23 -0300

    refactor: Atualiza mock para data

--- mscmcldap/api/mocks/reuniao.py
+++ mscmcldap/api/mocks/reuniao.py
@@ -1,31 +1,40 @@
 import datetime
 from .data import REUNIOES
 
+
+def hoje_str():
+    return datetime.datetime.now().strftime("%d/%m/%Y")
+
+
 def get_reuniao_mock(rec_id):
   rec_id = int(rec_id)
+
   for r in REUNIOES:
     if r['rec_id'] == rec_id:
       return {
         'rec_id': r['rec_id'],
         'con_id': r['con_id'],
-        'con_desc': r['con_desc'], 
+        'con_desc': r['con_desc'],
         'con_sigla': r['con_sigla'],
         'rec_tipo_reuniao': r['rec_tipo_reuniao'],
         'rec_numero': r['rec_numero'],
-        'versao': '1.0',  
-        'rec_data': r['rec_data'],
+        'versao': '1.0',
+        'rec_data': hoje_str(),
         'pac_id': r['pac_id']
       }
+
   return None
+
 
 def get_rec_id_mock(pac_id):
   pac_id = int(pac_id)
+
   for r in REUNIOES:
     if r['pac_id'] == pac_id:
       return [
         {
-          'rec_id': r['rec_id'],
+            'rec_id': r['rec_id'],
         }
       ]
-  return None
 
+  return None
